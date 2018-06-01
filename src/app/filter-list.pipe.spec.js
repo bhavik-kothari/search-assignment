a46c722fc231f0f2f27cbdd6471626295bf859be
Bhavik Kothari <bkothari@mobiquityinc.com>
2018-06-01 16:35:14 +0530

    added files

---
+++ src/app/filter-list.pipe.spec.js
@@ -0,0 +1,9 @@
+"use strict";
+var filter_list_pipe_1 = require('./filter-list.pipe');
+describe('FilterListPipe', function () {
+    it('create an instance', function () {
+        var pipe = new filter_list_pipe_1.FilterListPipe();
+        expect(pipe).toBeTruthy();
+    });
+});
+//# sourceMappingURL=filter-list.pipe.spec.js.map
